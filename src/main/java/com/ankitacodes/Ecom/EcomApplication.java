e7a8ec537d17e47cf391e270112fd52f9f1ea920
Ankita <ankitanandankar@gmail.com>
2023-04-06 17:06:08 +0530

    Added then commented annotation for active profiles

--- src/main/java/com/ankitacodes/Ecom/EcomApplication.java
+++ src/main/java/com/ankitacodes/Ecom/EcomApplication.java
@@ -2,8 +2,10 @@ package com.ankitacodes.Ecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+//@Profile(value = "prod")
 public class EcomApplication {
 
 	public static void main(String[] args) {
