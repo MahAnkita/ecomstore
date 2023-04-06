5fcc04a8c2d4c299ecfacba640cc41ea500c3838
Ankita <ankitanandankar@gmail.com>
2023-04-06 17:25:15 +0530

    created productserviceImpl class to override methods in this impl class

--- src/main/java/com/ankitacodes/Ecom/service/ProductService.java
+++ src/main/java/com/ankitacodes/Ecom/service/ProductService.java
@@ -1,4 +1,16 @@
 package com.ankitacodes.Ecom.service;
 
+import com.ankitacodes.Ecom.payloads.ProductDto;
+
+import java.util.List;
+
 public interface ProductService {
+
+    public ProductDto create(ProductDto productDto);
+    public ProductDto update(ProductDto productDto,Long productId);
+    public void delete(Long productId);
+    public ProductDto getProductById(Long productId);
+    public List<ProductDto> getAll();
+    public List<ProductDto> searchByTitle(String title);
+    public List<ProductDto> searchByLive(boolean live);
 }
